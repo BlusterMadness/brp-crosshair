8af3587b0008eb5aae5b6036e0933cfc286ed058
Bluster Madness <99558013+BlusterMadness@users.noreply.github.com>
2024-10-26 17:49:42 -0400

    Add files via upload

---
+++ brp-crosshair/client/client.lua
@@ -0,0 +1,21 @@
+CreateThread(
+    function()
+        while true do
+            Wait(0)
+            if IsPedArmed(PlayerPedId(), 4 | 2) then
+                if (IsPlayerFreeAiming(PlayerId())) then
+                    SendNUIMessage({display = "crosshairShow"})
+                    Wait(100)
+                else
+                    SendNUIMessage({display = "crosshairHide"})
+                    Wait(100)
+                end
+            else
+                SendNUIMessage({display = "crosshairHide"})
+                Wait(1000)
+            end
+        end
+    end
+)
+
+SetPedConfigFlag(PlayerPedId(), 438, true)
